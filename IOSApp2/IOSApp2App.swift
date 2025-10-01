24700dc1d447db043ea79e820ce6320a1ec2018d
JoseFlores <joseolmedo.florespaniagua@triosstudent.com>
2025-10-01 10:26:34 -0400

    Initial Commit

---
+++ IOSApp2/IOSApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  IOSApp2App.swift
+//  IOSApp2
+//
+//  Created by Jose Flores on 2025-10-01.
+//
+
+import SwiftUI
+
+@main
+struct IOSApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
